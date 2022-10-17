9dcf546c5fdc8df5a314967f32f6602782b1a5da
Quentin Haemmerlé <uyxu8mqx@protonmail.com>
2022-10-17 23:05:51 +0200

    Rend un chiffre à partir d'une chaîne de caractères

--- scripts/numerologie.js
+++ scripts/numerologie.js
@@ -17,3 +17,42 @@ for (c of "cou") {
     console.log(c + " : " + nombre(c))
 }
 // fin de test de nombre(chaine)
+
+function somme(nombre) {
+    var somme = 0
+    chaine = String(nombre)
+    for (var i=0; i < chaine.length ; i++) {
+        somme += parseInt(chaine.charAt(i))
+    }
+    return somme
+}
+
+// test de somme(nombre)
+console.log(somme(132))
+
+// avec un chiffre : charAt != charCodeAt
+console.log(somme(4))
+console.log("4".charCodeAt(0))
+console.log("4".charAt(0))
+
+// conversion chaine de caractères et nombre
+console.log(typeof "4".charAt(0))
+console.log(parseInt("4".charAt(0)))
+console.log(typeof parseInt("4".charAt(0)))
+// fin de test de somme(nombre)
+
+function chiffreAssocie(chaine) {
+    valeur = nombre(chaine)
+
+    while (valeur > 9) {
+        valeur = somme(valeur)
+    }
+    return valeur
+}
+
+// test de chiffreAssocie(chaine)
+
+//test valeur somme des chiffres
+console.log(nombre("coucou"))
+console.log(chiffreAssocie("coucou"))
+// fin de test de chiffreAssocie(chaine)
